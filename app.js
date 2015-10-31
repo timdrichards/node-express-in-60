fde6a33ef09846229a4eabe6bd8b823f7d1337c5
Tim Richards <tim.d.richards@gmail.com>
2015-10-31 11:23:12 -0400

    finished

--- app.js
+++ app.js
@@ -27,14 +27,54 @@ app.set('view engine', 'handlebars');
 // requested. This is how you create a new Array in JavaScript.
 var images = [];
 
-// The `timesHandler` will handle HTTP requests. It dynamically
-// generates k images, where k is the number of times this route has
-// been requested by a browser.
+// This is a rewrite of our original times handler to use a view
+// instead of dynamically generating HTML in JavaScript.
 function timesHandler(req, res) {
-  
+  // The `render` method is added by the handlebars middleware. The
+  // first argument is the name of the view. You need not indicate
+  // the full path and you can drop the extension. The second argument
+  // is a "context" that is passed into the view template to fill in
+  // the holes.
+  res.render('times-view', {
+    visits: images.length,
+    images: images
+  });
 }
 
+// The cat handler pushes a new object with properties for the image
+// that was requested and the date it was requested on. It then
+// renders the view with the given array of image objects to be filled
+// in by the template.
+function timesCatHandler(req, res) {
+  images.push({
+    url: '/img/mypic.jpg',
+    date: new Date()
+  });
+  res.render('times-view', {
+    visits: images.length,
+    images: images
+  });
+}
+
+// The dog handler pushes a new object with properties for the image
+// that was requested and the date it was requested on. It then
+// renders the view with the given array of image objects to be filled
+// in by the template.
+function timesDogHandler(req, res) {
+  images.push({
+    url: '/img/dog.jpg',
+    date: new Date()
+  });
+  res.render('times-view', {
+    visits: images.length,
+    images: images
+  });
+}
+
+// Here, we bind our routes to route handlers.
 app.get('/times', timesHandler);
+app.get('/times/cat', timesCatHandler);
+app.get('/times/dog', timesDogHandler);
 
 //////////////////////////////////////////////////////////////////////
 
